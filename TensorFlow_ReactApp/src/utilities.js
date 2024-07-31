8b1af676f90d8a87926ed116ba4ae5705ccf73b5
dyells07 <bhimkhanal999@gmail.com>
2024-07-31 23:24:43 +0545

    Struct : Obeject Detection Setup for Object detection

---
+++ TensorFlow_ReactApp/src/utilities.js
@@ -0,0 +1,30 @@
+// Define our labelmap
+const labelMap = {
+    1:{name:'ThumbsUp', color:'red'},
+    2:{name:'ThumbsDown', color:'yellow'},
+    3:{name:'ThankYou', color:'lime'},
+    4:{name:'LiveLong', color:'blue'},
+}
+
+// Define a drawing function
+export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx)=>{
+    for(let i=0; i<=boxes.length; i++){
+        if(boxes[i] && classes[i] && scores[i]>threshold){
+            // Extract variables
+            const [y,x,height,width] = boxes[i]
+            const text = classes[i]
+            
+            // Set styling
+            ctx.strokeStyle = labelMap[text]['color']
+            ctx.lineWidth = 10
+            ctx.fillStyle = 'white'
+            ctx.font = '30px Arial'         
+            
+            // DRAW!!
+            ctx.beginPath()
+            ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100)/100, x*imgWidth, y*imgHeight-10)
+            ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/2, height*imgHeight/2);
+            ctx.stroke()
+        }
+    }
+}
